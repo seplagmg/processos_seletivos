4cd6be786e44d95de16c2f28d3086730abfd6513
Romão Ferreira <romao2000@gmail.com>
2025-03-31 11:22:49 -0300

    Teste de alteração

--- application/controllers/Candidatos.php
+++ application/controllers/Candidatos.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Candidatos extends CI_Controller {
+        
         function __construct() {
                 parent::__construct();
                 $this -> load -> model('Candidatos_model');
